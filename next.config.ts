478443b5b23a87ac20d325661b9ce043841ec827
minkhoaa <tukhoa040505@gmail.com>
2025-12-23 22:17:39 +0700

    update source image

--- next.config.ts
+++ next.config.ts
@@ -25,6 +25,28 @@ const nextConfig: NextConfig = {
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.wp.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.wordpress.com",
+      },
+      {
+        protocol: "https", 
+        hostname: "**",  // Allow all domains - you can restrict this later
+      },
+       {
+        protocol: "http",
+        hostname: "images.mini-ielts.com",
+      },
+      {
+        protocol: "https", 
+        hostname: "images.mini-ielts.com",
+      },
+    ],
   },
 };
 
